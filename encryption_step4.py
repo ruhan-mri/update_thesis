556b2991df0e2273240d2693b84d80c608712c95
Ruhan Islam <ruhan.9mri@gmail.com>
2023-11-05 15:13:42 +0600

    some portion of setup4

--- encryption_step4.py
+++ encryption_step4.py
@@ -1,6 +1,11 @@
 import random
 import generator
+import key_setup1
 import key_setup2
+import encryption_step1
+import encryption_step2
+from integer_pairing import cantor, szudzik
+
 
 p = generator.p
 g1  = generator.g1
@@ -13,3 +18,13 @@ Is = pow(g1, s, p)
 I_dash = pow(key_setup2.Du, -r, p)
 Io = pow(g1, r, p)
 
+# cantor.pair(11, 13) # 313
+# cantor.unpair(313) # (11, 13)
+beta = cantor.pair(key_setup1.Dc, key_setup1.Qc)
+beta = pow(beta, -s, p)
+beta = (r * beta) % p
+Ii = (key_setup2.Du * pow(g1, encryption_step1.encrypted_keywords[0], p)) % p
+Ii = pow(Ii, beta , p)
+
+# I_bar = encryption_step2.encrypt_message(beta)
+print(Ii)
